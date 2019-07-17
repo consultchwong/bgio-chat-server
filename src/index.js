5dc2439b7caa9bf26a0bb3006a911896b560deba
consultchwong <consultchwong@gmail.com>
2019-07-17 18:24:53 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import "babel-polyfill";
 import { Server } from "./libs/boardgame.io/server";
 import  Game  from "./game";
+import  PlayerProfile  from "./playerprofile";
 
 console.log(Game);
 
 const port = process.env.PORT || 8000;
-const server = Server({ games: [Game] });
+const server = Server({ games: [Game, PlayerProfile] });
 
 server.run(port, () => {
   console.log("App is running on port " + port);
